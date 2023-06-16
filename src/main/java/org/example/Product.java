c52ca0e9569836e0a8ea4694d15128c916f401f0
Patrick Johnson <jpatrick@vmware.com>
2023-06-16 14:22:40 -0700

    Using Boot, breaks with aot enabled

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -2,7 +2,5 @@ package org.example;
 
 import org.springframework.data.annotation.Id;
 
-import java.io.Serializable;
-
-public record Product(@Id String name, float price, int quantity) implements Serializable {
+public record Product(@Id String name, float price, int quantity) {
 }
