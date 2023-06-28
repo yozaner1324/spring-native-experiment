82d283a59f2bd9787834ff5df66173b64094ed2c
Patrick Johnson <jpatrick@vmware.com>
2023-06-28 10:08:24 -0700

    imports

--- src/main/java/org/example/ApplicationController.java
+++ src/main/java/org/example/ApplicationController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 @RestController
 public class ApplicationController {
